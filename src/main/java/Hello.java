3ffd89d7102460818317aefb6b038a846a4362a8
shymad12 <128215854+shymad12@users.noreply.github.com>
2023-07-22 09:36:14 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,9 +1,7 @@
-// Import required java libraries
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-// Extend HttpServlet class
 public class Hello extends HttpServlet {
 
    private String message;
@@ -24,8 +22,8 @@ public class Hello extends HttpServlet {
          PrintWriter out = response.getWriter();
          out.println("<h1>" + message + "</h1>");
       } catch (IOException e) {
-         // Handle the IOException here (e.g., logging the error or sending an error response)
-         e.printStackTrace();
+         // Log the error using a logger
+         LOGGER.error("An IOException occurred", e);
       }
    }
 }
