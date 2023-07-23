722f97bfbd0645e1ffe3b8154122633d4ed98082
shymad12 <128215854+shymad12@users.noreply.github.com>
2023-07-23 10:52:14 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -9,27 +9,28 @@ import javax.servlet.http.HttpServletResponse;
 
 public class Hello extends HttpServlet {
 
-   private String message;
-   private static final Logger LOGGER = LoggerFactory.getLogger(Hello.class);
+    private String message;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Hello.class);
 
-   public void init() throws ServletException {
-      // Do required initialization
-      message = "Hello from Shyam";
-   }
+    public void init() throws ServletException {
+        // Do required initialization
+        message = "Hello from Shyam";
+    }
 
-   public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Set response content type
+        response.setContentType("text/html");
 
-      // Set response content type
-      response.setContentType("text/html");
-
-      try {
-         // Actual logic goes here.
-         PrintWriter out = response.getWriter();
-         out.println("<h1>" + message + "</h1>");
-      } catch (IOException e) {
-         // Log the error using the logger
-         LOGGER.error("An IOException occurred", e);
-      }
-   }
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<h1>" + message + "</h1>");
+        } catch (IOException e) {
+            // Log the error using the logger
+            LOGGER.error("An IOException occurred", e);
+        } catch (Exception e) {
+            // Handle any other exceptions appropriately
+            LOGGER.error("An unexpected error occurred", e);
+        }
+    }
 }
